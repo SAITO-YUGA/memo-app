4ae95eac61711e7866c20576c9ab226d339bf931
Yuga Saito <yuga@YuganoMacBook-Air.local>
2025-09-11 15:09:19 +0900

    DBにテーブルを作成

---
+++ laravel/database/seeders/NotesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class NotesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('notes')->insert([
+            ['title' => '初期', 'content' => 'あ']
+        ]);
+    }
+}
